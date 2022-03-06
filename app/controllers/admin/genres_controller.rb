f16262566ef5852e655badb126ad3967e5504582
njguld95038043 <t.tamura6060@gmail.com>
2022-03-06 09:49:28 +0000

    [Add]管理者側ジャンル一覧画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,4 +1,24 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    @genre = Genre.find(params[:id])
+    @genre.destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
