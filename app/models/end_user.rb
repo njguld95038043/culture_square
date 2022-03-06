0fae99f12443aaa7577cb4fde0414f5ce543ff08
njguld95038043 <t.tamura6060@gmail.com>
2022-03-06 05:07:21 +0000

    [Update]新規登録画面

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -4,6 +4,7 @@ class EndUser < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum gender: { man: 1, woman: 2, other: 3}
+
   has_one_attached :profile_image
 
 end
