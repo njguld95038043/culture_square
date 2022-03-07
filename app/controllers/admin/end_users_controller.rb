c87cad4880cbcccc95f0a5e1a9b38589453a95ff
njguld95038043 <t.tamura6060@gmail.com>
2022-03-07 04:26:04 +0000

    [Add]Adminのユーザ情報編集画面

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -8,5 +8,18 @@ class Admin::EndUsersController < ApplicationController
   end
 
   def edit
+    @end_user = EndUser.find(params[:id])
+  end
+
+  def update
+    @end_user = EndUser.find(params[:id])
+    @end_user.update(end_user_params)
+    redirect_to admin_end_user_path(@end_user)
+  end
+
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:nick_name, :birthday, :gender, :occupation, :email, :is_deleted)
   end
 end
