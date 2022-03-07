097d4dea0793de413afff5a41a7930fa2c832cec
njguld95038043 <t.tamura6060@gmail.com>
2022-03-07 05:20:50 +0000

    [Add]ユーザー情報編集画面

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,10 +1,24 @@
 class Public::EndUsersController < ApplicationController
   def show
+    @end_user = current_end_user
   end
 
   def edit
+    @end_user = current_end_user
+  end
+
+  def update
+    @end_user = EndUser.find(params[:id])
+    @end_user.update(end_user_params)
+    redirect_to end_users_my_page_path
   end
 
   def unsubscribe
+  end
+
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:nick_name, :birthday, :gender, :occupation, :email, :is_deleted)
   end
 end
