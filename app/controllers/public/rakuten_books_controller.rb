3a2b811bde8c9c561499a46249efbde3bc7d1066
njguld95038043 <t.tamura6060@gmail.com>
2022-03-09 05:35:50 +0000

    [Update]Topページ/[Add]pry-byebug

--- app/controllers/public/rakuten_books_controller.rb
+++ app/controllers/public/rakuten_books_controller.rb
@@ -3,6 +3,20 @@ class Public::RakutenBooksController < ApplicationController
     if params[:keyword]
       @rakuten_books = RakutenWebService::Books::Book.search(title: params[:keyword])
     end
+    @book = Book.new
+  end
+
+  def create
+    binding.pry
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to new_review_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :isbn, :sales_date, :item_price, :item_caption, :publisher_name, :small_image_url, :medium_image_url, :large_image_url, :item_url)
   end
 
 end
