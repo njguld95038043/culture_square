16f05b06b2dffc3f5523beb67ac35171fc4a4fcc
njguld95038043 <t.tamura6060@gmail.com>
2022-03-11 09:38:53 +0000

    [Add]いいね機能の非同期通信

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,15 @@
 class Public::FavoritesController < ApplicationController
 
   def create
-    review = Review.find(params[:review_id])
-    favorite = current_end_user.favorites.new(review_id: review.id)
-    favorite.save
-    redirect_to request.referer
+    @review = Review.find(params[:review_id])
+    @favorite = current_end_user.favorites.new(review_id: @review.id)
+    @favorite.save
   end
 
   def destroy
-    review = Review.find(params[:review_id])
-    favorite = current_end_user.favorites.find_by(review_id: review.id)
-    favorite.destroy
-    redirect_to request.referer
+    @review = Review.find(params[:review_id])
+    @favorite = current_end_user.favorites.find_by(review_id: @review.id)
+    @favorite.destroy
   end
 
 end
