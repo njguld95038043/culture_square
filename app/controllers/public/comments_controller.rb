68fbfe4ed4050a97799e5cc08c916d72f4eda06c
njguld95038043 <t.tamura6060@gmail.com>
2022-03-11 10:40:05 +0000

    [Add]コメント機能の非同期通信

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,16 +1,14 @@
 class Public::CommentsController < ApplicationController
 
   def create
-    review = Review.find(params[:review_id])
-    comment = current_end_user.comments.new(comment_params)
-    comment.review_id = review.id
-    comment.save
-    redirect_to request.referer
+    @review = Review.find(params[:review_id])
+    @comment = current_end_user.comments.new(comment_params)
+    @comment.review_id = @review.id
+    @comment.save
   end
 
   def destroy
     Comment.find(params[:id]).destroy
-    redirect_to request.referer
   end
 
   private
