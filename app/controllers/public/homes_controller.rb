26ae2eb1de479e6e1b7f2a75d527e5bc8f6a151f
njguld95038043 <t.tamura6060@gmail.com>
2022-03-12 08:17:00 +0000

    [Update]Topページレイアウト調整

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,7 @@ class Public::HomesController < ApplicationController
   def top
     @end_user = current_end_user
     @genres = Genre.all
+    @random_reviews = Review.all.shuffle
   end
 
   def about
