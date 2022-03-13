2ed2c595f9f432e7627ff7c481e6f71fc40b1b1b
njguld95038043 <t.tamura6060@gmail.com>
2022-03-13 07:59:19 +0000

    [Add]skippr導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,15 +7,18 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "script.js"
+import "script"
 
 import "jquery";
+import "skippr.min";
+import "raty";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
-import 'jquery'
 //import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+window.$ = window.jQuery = require('jquery')
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
-
 const webpack = require('webpack')
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
+      "window.jQuery": "jquery",
     Popper: 'popper.js'
   })
 )
+
+module.exports = environment
