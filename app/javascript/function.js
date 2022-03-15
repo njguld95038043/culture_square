ed6e2dafa1593211753b675f0b3b05d9cf707cf0
njguld95038043 <t.tamura6060@gmail.com>
2022-03-15 12:44:21 +0000

    [Update]Vegas表示可能にした

--- app/javascript/function.js
+++ app/javascript/function.js
@@ -1,9 +1,16 @@
-$('body').vegas({
-  slides: [
-    { src: 'images/book1.jpg' },
-    { src: 'images/book2.jpg' },
-    { src: 'images/book03.jpg' },
-    { src: 'images/book4.jpg' },
-  ],
-   transition: 'fade'
-});
+import book1 from 'images/book1.jpg'
+import book2 from 'images/book2.jpg'
+import book3 from 'images/book3.jpg'
+import book4 from 'images/book4.jpg'
+
+$(function() {
+    $('#vegas-body').vegas({
+    slides: [
+        { src: book1 },
+        { src: book2 },
+        { src: book3 },
+        { src: book4 },
+
+    ]
+  });
+})
