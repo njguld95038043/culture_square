95e52b4a58b222e7d940c4c0742967f429b538d2
njguld95038043 <t.tamura6060@gmail.com>
2022-03-15 02:19:25 +0000

    [Update]レビュー詳細ページ

--- app/javascript/function.js
+++ app/javascript/function.js
@@ -1,8 +1,11 @@
-$('body').vegas({
-  slides: [
-    { src: 'images/book1.jpg' },
-    { src: 'images/book2.jpg' },
-    { src: 'images/book3.jpg' },
-  ],
-   transition: 'fade'
+$(function() {
+    $('body').vegas({
+        slides: [
+            { src: 'images/book1.jpg' },
+            { src: 'images/book2.jpg' },
+            { src: 'images/book3.jpg' },
+            { src: 'images/book4.jpg' }
+        ],
+        transition: 'fade'
+    });
 });
