f1ed9877e5ebb2b6df21c5e97665eb7a161a6e68
njguld95038043 <t.tamura6060@gmail.com>
2022-03-16 00:57:50 +0000

    [Update]Vegasアニメーション調整＆レビュー詳細ページコメント機能調整

--- app/javascript/function.js
+++ app/javascript/function.js
@@ -3,14 +3,17 @@ import book2 from 'images/book2.jpg'
 import book3 from 'images/book3.jpg'
 import book4 from 'images/book4.jpg'
 
-$(function() {
+$(document).on('turbolinks:load', function () {
     $('#vegas-body').vegas({
     slides: [
         { src: book1 },
         { src: book2 },
         { src: book3 },
         { src: book4 },
-
-    ]
+    ],
+    transition: [ 'blur2' ],
+    animation: 'kenburnsDown',
+    delay: 10000,
+    animationDuration: 10000,
   });
 })
