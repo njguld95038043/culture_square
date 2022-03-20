97ba099e693861c89746d1eae469cf658f5299d9
njguld95038043 <t.tamura6060@gmail.com>
2022-03-20 01:47:27 +0000

    [Update]検索ページ

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -6,10 +6,13 @@ class Public::SearchesController < ApplicationController
 
     if @range == "EndUser"
       @end_users = EndUser.looks(params[:search], params[:word])
+      @nick_name = params[:word]
     elsif @range == "Review"
       @reviews = Review.looks(params[:search], params[:word])
+      @review = params[:word]
     else
       @books = Book.looks(params[:search], params[:word])
+      @book = params[:word]
     end
   end
 end
