d7cf6d5bbe23a8c64f9dd4eee7cc2a99b134b234
njguld95038043 <t.tamura6060@gmail.com>
2022-03-23 13:13:20 +0000

    [Update]ページネーション多言語化

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -6,13 +6,13 @@ class Public::SearchesController < ApplicationController
       @range = params[:range]
 
       if @range == "EndUser"
-        @end_users = EndUser.looks(params[:search], params[:word])
+        @end_users = EndUser.looks(params[:search], params[:word]).page(params[:page]).per(10)
         @nick_name = params[:word]
       elsif @range == "Review"
-        @reviews = Review.looks(params[:search], params[:word])
+        @reviews = Review.looks(params[:search], params[:word]).page(params[:page]).per(10)
         @review = params[:word]
       else
-        @books = Book.looks(params[:search], params[:word])
+        @books = Book.looks(params[:search], params[:word]).page(params[:page]).per(10)
         @book = params[:word]
       end
   end
