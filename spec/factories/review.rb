ac571fd9d293d9a13fdefff7ef09e1a413594d1c
njguld95038043 <t.tamura6060@gmail.com>
2022-04-12 02:05:29 +0000

    [Update]review_spec.rb

--- spec/factories/review.rb
+++ spec/factories/review.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :review do
     review { Faker::Lorem.characters(number:5) }
-    rate { Faker::Lorem.characters(number:20) }
+    rate { Faker::Lorem.characters(3.5) }
   end
 end
