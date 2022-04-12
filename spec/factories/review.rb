4e05124bd66cd03e37d373b1c2aca4034cc6a262
njguld95038043 <t.tamura6060@gmail.com>
2022-04-12 12:14:03 +0000

    [Update]RSpec

--- spec/factories/review.rb
+++ spec/factories/review.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
+  factory :end_user do
+    nick_name { "test" }
+    email { "test@example.com" }
+    gender { 1 }
+    occupation { "test" }
+    birthday { "1990-1-1" }
+    password { "testtest" }
+    password_confirmation { "testtest" }
+  end
   factory :review do
     review { Faker::Lorem.characters(number:5) }
     rate { Faker::Lorem.characters(3.5) }
--- spec/system/reviews_spec.rb
+++ spec/system/reviews_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
-describe '投稿のテスト' do
-  let!(:review) { create(:review,review:'hoge',rate:'3.5') }
+describe '新規登録のテスト' do
   describe 'トップ画面(root_path)のテスト' do
     before do
       visit root_path
     end
-    context '表示の確認' do
-      it 'root_pathが"/"であるか' do
-        expect(current_path).to eq('/')
-      end
+    it 'root_pathが"/"であるか' do
+      expect(current_path).to eq('/')
+      expect(page).to have_content('Sign up')
+    end
+  end
+  describe '新規登録画面のテスト' do
+    before do
+      visit root_path
+    end
+    it 'Sing upがあるか' do
+      expect(page).to have_content('Sign up')
     end
   end
 end
